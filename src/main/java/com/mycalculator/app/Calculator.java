f7ada2c048992ae09179fbea0175a0fed4fa8b11
JonaaaDev <jonathangimenezdev@gmail.com>
2025-11-27 07:09:03 +0100

    refactor: Update Calculator methods to use double and launch GUI

--- src/main/java/com/mycalculator/app/Calculator.java
+++ src/main/java/com/mycalculator/app/Calculator.java
@@ -2,26 +2,27 @@ package com.mycalculator.app;
 
 import java.util.Random;
 import java.util.Scanner;
+import javax.swing.SwingUtilities;
 
 public class Calculator {
 
-    public int add(int a, int b) {
+    public double add(double a, double b) {
         return a + b;
     }
 
-    public int subtract(int a, int b) {
+    public double subtract(double a, double b) {
         return a - b;
     }
 
-    public int multiply(int a, int b) {
+    public double multiply(double a, double b) {
         return a * b;
     }
 
-    public double divide(int a, int b) {
+    public double divide(double a, double b) {
         if (b == 0) {
             throw new IllegalArgumentException("No se puede dividir por cero.");
         }
-        return (double) a / b;
+        return a / b;
     }
 
     static class Mission {
@@ -52,45 +53,15 @@ public class Calculator {
 
         if (random.nextBoolean()) {
             operator = '+';
-            correctAnswer = add(operand1, operand2);
+            correctAnswer = (int) add(operand1, operand2);
         } else {
             operator = '-';
-            correctAnswer = subtract(operand1, operand2);
+            correctAnswer = (int) subtract(operand1, operand2);
         }
         return new Mission(operand1, operand2, operator, correctAnswer);
     }
 
     public static void main(String[] args) {
-        System.out.println("¡Bienvenido a las Misiones de la Calculadora Java!");
-        Scanner scanner = new Scanner(System.in);
-        Calculator myCalculator = new Calculator();
-        int score = 0;
-        int totalMissions = 3;
-
-        for (int i = 0; i < totalMissions; i++) {
-            Mission currentMission = myCalculator.generateMission();
-            System.out.println("
-Misión " + (i + 1) + ": " + currentMission.toString());
-            System.out.print("Tu respuesta: ");
-
-            try {
-                int userAnswer = scanner.nextInt();
-                if (userAnswer == currentMission.correctAnswer) {
-                    System.out.println("¡Correcto!");
-                    score++;
-                } else {
-                    System.out.println("Incorrecto. La respuesta correcta era: " + currentMission.correctAnswer);
-                }
-            } catch (java.util.InputMismatchException e) {
-                System.out.println("Entrada no válida. Por favor, introduce un número.");
-                scanner.next();
-                i--;
-            }
-        }
-
-        System.out.println("
---- Fin de las Misiones ---");
-        System.out.println("Tu puntuación final: " + score + "/" + totalMissions);
-        scanner.close();
+        SwingUtilities.invokeLater(() -> new CalculatorGUI());
     }
 }
