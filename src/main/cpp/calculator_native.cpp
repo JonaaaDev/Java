ec5531c185aedf355c3aa06b709cd13c00e08092
JonaaaDev <jonathangimenezdev@gmail.com>
2025-11-27 08:03:18 +0100

    .

--- src/main/cpp/calculator_native.cpp
+++ src/main/cpp/calculator_native.cpp
@@ -1,11 +1,6 @@
 #include <jni.h>
 #include <stdio.h>
 
-/*
- * Header for class com_mycalculator_app_Calculator
- * Naming convention: Java_Package_Class_Method
- */
-
 extern "C" {
 
     JNIEXPORT jdouble JNICALL Java_com_mycalculator_app_Calculator_addNative(JNIEnv *env, jobject obj, jdouble a, jdouble b) {
@@ -22,7 +17,6 @@ extern "C" {
 
     JNIEXPORT jdouble JNICALL Java_com_mycalculator_app_Calculator_divideNative(JNIEnv *env, jobject obj, jdouble a, jdouble b) {
         if (b == 0) {
-            // Throwing Java Exception from C++
             jclass exClass = env->FindClass("java/lang/IllegalArgumentException");
             env->ThrowNew(exClass, "No se puede dividir por cero (Desde C++)");
             return 0;
